2d31497e0f0260f11ac973235423ce6b7695bfca
daisies7779 <41288557+daisies7779@users.noreply.github.com>
2020-10-21 14:02:23 +0300

    Update SakartotSkaitlus.c

--- SakartotSkaitlus.c
+++ SakartotSkaitlus.c
@@ -1,15 +1,57 @@
 #include <stdio.h>
 
-int main()
+int
+main ()
 {
-    int a, b, c;
-    printf("Ievadiet pirmo veselo skaitli:\n");
-    scanf("%d", &a);
-    printf("Ievadiet otro veselo skaitli:\n");
-    scanf("%d", &b);
-    printf("Ievadiet treso veselo skaitli:\n");
-    scanf("%d", &c);
-    printf("Jūs ievadījās skaiļus %d, %d, %d \n", a, b, c);
-    printf("Ja velaties sakartot augosa seciba ievadiet A, ja dilstosa - D");
-    return 0;
-}
+  int a, b, c, s;
+
+  printf ("Ievadiet pirmo veselo skaitli:\n");
+  scanf ("%d", &a);
+  printf ("Ievadiet otro veselo skaitli:\n");
+  scanf ("%d", &b);
+  printf ("Ievadiet treso veselo skaitli:\n");
+  scanf ("%d", &c);
+  printf ("JE+s ievadD+jDs skaiD<us %d, %d, %d \n", a, b, c);
+  printf
+    ("Ja velaties sakartot augosa seciba ievadiet 1, ja dilstosa - 0 \n");
+  scanf ("%d", &s);
+  if (s == 1)
+    {
+      if (a >= b)
+	{
+	  if (b >= c)
+	    {
+	      printf ("Skaitli sakarotit augusi %d, %d, %d", a, b, c);
+
+	    }
+	  else if (b > a)
+	    {
+	      if (a >= c)
+		{
+		  printf ("Skaitli sakarotit augusi %d, %d, %d", b, a, c);
+
+		}
+	    }
+
+	  else if (c >= a)
+	    {
+	      if (a >= b)
+		{
+		  printf ("Skaitli sakarotit augusi %d, %d, %d", c, a, b);
+
+		}
+	    }
+	  else if (c >= b)
+	    {
+	      if (b >= a)
+		{
+		  printf ("Skaitli sakarotit augusi %d, %d, %d", c, b, a);
+
+		}
+	    }
+	}
+
+    }
+	printf ("beigas \n");
+      return 0;
+    }
