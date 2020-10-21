9dddd37ec79d553c6f4d16745c7fd235149f61f0
daisies7779 <41288557+daisies7779@users.noreply.github.com>
2020-10-21 13:22:43 +0300

    Create SakartotSkaitlus.c
    21.septembra nodarbiba

---
+++ SakartotSkaitlus.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    printf("Ievadiet pirmo veselo skaitli:\n");
+    scanf("%d", &a);
+    printf("Ievadiet otro veselo skaitli:\n");
+    scanf("%d", &b);
+    printf("Ievadiet treso veselo skaitli:\n");
+    scanf("%d", &c);
+    printf("Jūs ievadījās skaiļus %d, %d, %d \n", a, b, c);
+    printf("Ja velaties sakartot augosa seciba ievadiet A, ja dilstosa - D");
+    return 0;
+}
