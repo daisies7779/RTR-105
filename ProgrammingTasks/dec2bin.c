618baffa9935c2a9323eea05e6e7e5a1e2850246
daisies7779 <41288557+daisies7779@users.noreply.github.com>
2020-10-26 00:03:33 +0200

    Update dec2bin.c

--- ProgrammingTasks/dec2bin.c
+++ ProgrammingTasks/dec2bin.c
@@ -9,11 +9,12 @@ int main ()
 
 /* Bitwise >> operācija ir ekfivalenta dalījumam ar divi.
 operācija (atlikums & 1) ģenerēs 0, ja atlikums ir nulle, vai 1 ja atlikums ir viens.
-Lai realizētu dec to bin, ideja ir tajā, ka skaitļa bitus vajag cikliski pārbīdīt pa labi, katrā ciklā samazinot pārbīdāmo pozīciju daudzumu no 8 līdz 0
+Lai realizētu dec to bin, ideja ir tajā, ka skaitļa bitus vajag cikliski pārbīdīt pa labi, katrā ciklā samazinot pārbīdāmo pozīciju daudzumu no 7 līdz 0
+kopumā tādejādi būs veiktas 7+0 = 8 iterācijas, kas simbolizēs vienu baitu. Baits - tik liels ir char tipa decimālskaitlis.
 */	
 	
 	
-  for (pozicija = 8; pozicija >= 0; pozicija--)
+  for (pozicija = 7; pozicija >= 0; pozicija--)
     {
       atlikums = skaitlis >> pozicija;
 
