9b23c9d71b05708581d05d34580fa46b3eac1c2d
daisies7779 <41288557+daisies7779@users.noreply.github.com>
2020-10-25 23:59:53 +0200

    Update dec2bin.c

--- ProgrammingTasks/dec2bin.c
+++ ProgrammingTasks/dec2bin.c
@@ -4,7 +4,15 @@ int main ()
   char skaitlis, pozicija, atlikums;
 
   printf ("Ievadiet decimālskaitli\n");
+ // skaitlis deklarēts kā char, tāpēc tam jābūt robežā no -128 līdz 127	
   scanf ("%d", &skaitlis);
+
+/* Bitwise >> operācija ir ekfivalenta dalījumam ar divi.
+operācija (atlikums & 1) ģenerēs 0, ja atlikums ir nulle, vai 1 ja atlikums ir viens.
+Lai realizētu dec to bin, ideja ir tajā, ka skaitļa bitus vajag cikliski pārbīdīt pa labi, katrā ciklā samazinot pārbīdāmo pozīciju daudzumu no 8 līdz 0
+*/	
+	
+	
   for (pozicija = 8; pozicija >= 0; pozicija--)
     {
       atlikums = skaitlis >> pozicija;
