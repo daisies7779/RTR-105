4751ec6972162279ae6c422c2795efee48628c05
daisies7779 <41288557+daisies7779@users.noreply.github.com>
2020-10-25 20:46:20 +0200

    Update multiplication.c
    labota git ieladiesanas kluda

--- ProgrammingTasks/multiplication.c
+++ ProgrammingTasks/multiplication.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-int
-main ()
+int main ()
 {
   long int a, b;
   printf ("Ievadiet skaitli 400 000 tukstosi: \n");
   scanf ("%ld", &a);
   printf ("Ievadiet skaitli 100 000 tukstosi: \n");
-  scanf ("%longd", &b);
+  scanf ("%ld", &b);
   printf ("Skaitlu reizinajums ir vienads ar %ld: \n", b*a);
 
   return 0;
