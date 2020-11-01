ba348d109bcf09bb0bc892560448fb357533b2b3
daisies7779 <41288557+daisies7779@users.noreply.github.com>
2020-11-01 10:39:38 +0200

    Update simpleSort.c

--- ProgrammingTasks/simpleSort.c
+++ ProgrammingTasks/simpleSort.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main ()
 {
-  int a, b, c;
+  int a, b, c, seciba;
+  int token_big, token_middle, token_small;
 
   printf ("Ievadiet decimālskaitli\n");
 
@@ -17,12 +18,55 @@ int main ()
 	
 	
   printf ("Jūs ievadījāt, a = %d b = %d c = %d \n", a,b,c);
-  printf ("Lai kārtotu augošā secībā ievadiet skaitli 1, lai dilstošā 0");
+  printf ("Lai kārtotu augošā secībā ievadiet skaitli 1, lai dilstošā 0 \n");
   scanf ("%d", &seciba);
   
-  if (seciba) {
-      if (a>b>c) {printf ("%d %d %d", a, b, c)}
+  if (seciba==1) {
+      printf("true\n");
+      if (a >= b && b >= c) {
+          token_big = a;
+          token_middle = b;
+          token_small = c;
+      }
+      else if (a >= b && b < c) {
+          token_big = a;
+          token_middle = c;
+          token_small = b;
+          
+      } 
+      else if (b > a && a >= c) {
+          token_big = b;
+          token_middle = a;
+          token_small = c;
+          
+      }
+      else if (b > a && a < c) {
+          token_big = b;
+          token_middle = c;
+          token_small = a;
+          
+      }  
+      else if (c > a && a >= b) {
+          token_big = c;
+          token_middle = a;
+          token_small = b;
+          
+      }
+      else if (c > a && a < b) {
+          token_big = c;
+          token_middle = b;
+          token_small = a;
+          
+      }
   }
+  
+  else if (seciba==0)
+  {
+      
+  }
+  else {printf("Programmā ir ielavījusies kļūda!");}
+  
+  printf ("c =%d b=%d a=%d", token_big, token_middle, token_small);
 
   return 0;
 }
