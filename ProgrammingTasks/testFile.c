422361665f97d3d975eb753b9f12d656af8ff0a6
daisies7779 <41288557+daisies7779@users.noreply.github.com>
2020-11-09 00:13:49 +0200

    Update testFile.c

--- ProgrammingTasks/testFile.c
+++ ProgrammingTasks/testFile.c
@@ -1,18 +1,68 @@
-//Šis fails ir domāts testēšanai
+#include <stdio.h>
+int main() {
+    
+    // lietotaja ievadits deicmalskaitlis, tiek pienemts ka tas nebus parak liels, dēļ ierobežotās skaitļošanas jaudas
+    int i;
+    
+    // izvēlētais aprēķināmā faktoriāla datu tips
+    char tips;
+    
+    //ja mazāks par kluda, tad pec salidzinasanas operacijas, secinas ka ir kluda.
+    long long kluda = 0;
+    
+    printf("Ievadiet veselu decimālskaitli: ");
+    scanf("%d", &i);
+  
+    printf("Izvelieties faktoriala datu tipu - c (char), i (int), l (long long int): ");
+    scanf(" %c", &tips);
+  
+  
+   if (tips == 'c') 
+   {
+     char fact = 1;
+     printf("\nIegaja CHAR\n");
+     
+     for (i; i>=1; i--)
+     {
+       
+       printf("vertiba %d \n", fact);
+       fact *= i;
+       
+     }
+     
+  printf("\nFaktoriāls = %d",  fact);
+     
+   }
 
-#include <stdio.h> 
+  else if (tips == 'i') 
+  {
+      int fact = 1;
+      for (i; i>=1; i--)
+     {
+       printf("Iegaja INT");
+       fact *= i;
+       
+     }
+    
+  printf("\nFaktoriāls = %d",  fact);
+    
+  }
   
-// function to find factorial of given number 
-unsigned int factorial(unsigned int n) 
-{ 
-    if (n == 0) 
-        return 1; 
-    return n * factorial(n - 1); 
-} 
+   else if (tips == 'l') 
+  {
+     long long fact = 1;
+     for (i; i>=1; i--)
+     {
+       printf("Iegaja LONG");
+       fact *= i;
+       
+     }
+     
+  printf("\nFaktoriāls = %lld",  fact);
+    
+  }
   
-int main() 
-{ 
-    int num = 5; 
-    printf("Factorial of %d is %d", num, factorial(num)); 
-    return 0; 
+  else {printf("Ievadiet korektu datu formātu!");}
+  
+     return 0;
 }
