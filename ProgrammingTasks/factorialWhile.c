9f40a2878abe9562a16fd215226600da4c595a94
daisies7779 <41288557+daisies7779@users.noreply.github.com>
2020-11-08 21:04:45 +0200

    Create factorialWhile.c

---
+++ ProgrammingTasks/factorialWhile.c
@@ -0,0 +1,21 @@
+  
+#include <stdio.h>
+int main() {
+    int n, i;
+    unsigned long long fact = 1;
+    printf("Ievadiet veselu decimālskaitli: ");
+    scanf("%d", &n);
+
+    // rāda kļūdu, ja ievadīts negatīvs skaitlis
+    if (n < 0)
+        printf("Faktoriāls no negatīva skaitļa neeksistē");
+    else {
+        while (i<=n) {
+            fact *= i;
+            i++;
+        }
+        printf("Faktoriāls skaitlim %d = %llu", n, fact);
+    }
+
+    return 0;
+}
