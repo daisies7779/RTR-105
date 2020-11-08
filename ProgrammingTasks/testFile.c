4d334b1ed9484824d3bd8ba14f1977a7d7c83157
daisies7779 <41288557+daisies7779@users.noreply.github.com>
2020-11-08 21:30:34 +0200

    Create testFile.c

---
+++ ProgrammingTasks/testFile.c
@@ -0,0 +1,18 @@
+//Šis fails ir domāts testēšanai
+
+#include <stdio.h> 
+  
+// function to find factorial of given number 
+unsigned int factorial(unsigned int n) 
+{ 
+    if (n == 0) 
+        return 1; 
+    return n * factorial(n - 1); 
+} 
+  
+int main() 
+{ 
+    int num = 5; 
+    printf("Factorial of %d is %d", num, factorial(num)); 
+    return 0; 
+}
