b7792d3911fd90fbc42f44ce99dd1952752a22f7
daisies7779 <41288557+daisies7779@users.noreply.github.com>
2020-11-16 12:00:11 +0200

    Update testFile.c

--- ProgrammingTasks/testFile.c
+++ ProgrammingTasks/testFile.c
@@ -5,16 +5,27 @@ void noArgumentNoReturn(){
      printf ("1.No argument, no return!\n");
 }
 
-char *noArgumentWithReturn(){
-     char announcement[] = ("2.No argument, yes return! and returns exactly this text you read right now! \n");
-     return announcement;
+const char* noArgumentWithReturn(){
+     return ("2.No argument, yes return! and returns exactly this text you read right now! \n");
+     
     
 }
+
+void withArgumentNoReturn(char *myString){
+     printf("%s", myString);
+     
+    
+}
+
 
 int main() {
     
 
     noArgumentNoReturn();
-    noArgumentWithReturn()
+    
+    printf("%s", noArgumentWithReturn());
+    
+    char *argument = "3.Yes argument, no return! and tex you see is passed as a string argument to function! \n" ;
+    withArgumentNoReturn(argument);
     return 0;
 }
