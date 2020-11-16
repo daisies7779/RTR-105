8d528bffa2cd8cb69b499a0a13ccaf27db0316ce
daisies7779 <41288557+daisies7779@users.noreply.github.com>
2020-11-16 11:38:46 +0200

    Update testFile.c

--- ProgrammingTasks/testFile.c
+++ ProgrammingTasks/testFile.c
@@ -1,116 +1,20 @@
 #include <stdio.h>
+
+
+void noArgumentNoReturn(){
+     printf ("1.No argument, no return!\n");
+}
+
+char *noArgumentWithReturn(){
+     char announcement[] = ("2.No argument, yes return! and returns exactly this text you read right now! \n");
+     return announcement;
+    
+}
+
 int main() {
     
-    // lietotāja ievadīts decimālskaitlis
-    int i;
-    
-    // izvēlētais aprēķināmā faktoriāla datu tips
-    char tips;
-    
-    
-    printf("Ievadiet decimālskaitli, lielāku vai vienādu ar nulle: ");
-    scanf("%d", &i);
-    
-    //nav pretrunas ar uzdevuma nosacījumiem, jo FAKTORIĀLA vērtība ar nulli netiek salīdzināt. Tā vispār netiek rēķināta, 
-    //jo negatīvs skaitlis neitilpst definīcijas apgabalā.
-    if(i<0){printf("No negatīva skaitļa faktoriālu nerēķina!"); return 0;}
-  
-    printf("Izvelieties faktoriala datu tipu - c (char), i (int), l (long long int): ");
-    scanf(" %c", &tips);
-  
-  
-  
-  
-   if (tips == 'c') 
-   {
-     char fact = 1;
-     char ieprieks =1;
-     printf("\nIegaja CHAR\n");
-     printf("Rekinam fact skailim %d \n", i);
-     
-     // cikls rēķina fatkoriālu no beigām, lai ātrāk sasniegtu kritisko robežu
-     // tāpēc, piemēram, (n-1)! īstenībā nav faktoriāls bet tā atpakaļgaita, piem 9*8*7...
-     // tas nīzmē, ka kodā nav īsti korektu saukt fact par faktoriālu, kamēr  tas nav pilnībā izrēķināts,
-     //  taču šinī uzdevumā precīzai matemātiskai terminoloģijai nav principiālas nozīmes.
-     while (i>=1)
-     {
-       
-       
-      fact *= i;
-      printf("Vai aprēķinātā n! dalījums ar n ir vienāds ar (n-1)!? %d \n", (fact/i)==ieprieks);
-      // Ja aprēķinātā n! dalījums ar n NAV VIENĀDS ar (n-1)! tad ir pārsniegta datu tipa robeža
-      if ((fact/i)!=ieprieks) {printf("\nChar datu tipam faktoriālu nevar aprēķināt\n Zemak redzam kļūdainais aprkēķins"); break;}
-      printf("iepriekseja vertiba (n-1)! %d \n", ieprieks);    
-       ieprieks = fact;
-       printf("tekosa vertiba n! %d \n", fact);
-       i--;  
-       
-  
-       
-     }
-   //Arī kļūdas gadījumā rādīs aprēkināto vērtību, iemesls, kāpēc atstāju opciju redzēt vērtību arī   
-   // kļūdas gadījumā, lai var redzēt, ka tiešām kaut kas ne tā aizgāja.
-  printf("\nFaktoriāls = %d",  fact);
-     
-   }
 
-  else if (tips == 'i') 
-  {
-     int  fact = 1;
-     int ieprieks =1;
-     printf("\nIegaja INT\n");
-     printf("Rekinam fact skailim %d \n", i);
-     
-     
-     while (i>=1)
-     {
-      
-       fact *= i;
-      printf("Vai aprēķinātā n! dalījums ar n ir vienāds ar (n-1)!? %d \n", (fact/i)==ieprieks);
-      // Ja aprēķinātā n! dalījums ar n NAV VIENĀDS ar (n-1)! tad ir pārsniegta datu tipa robeža
-      if ((fact/i)!=ieprieks) {printf("\nInt datu tipam faktoriālu nevar aprēķināt \nZemak redzam kļūdainais aprkēķins"); break;}
-     printf("iepriekseja vertiba (n-1)! %d \n", ieprieks);    
-       ieprieks = fact;
-       printf("tekosa vertiba n! %d \n", fact);
-       i--;
-      
-       
-     }
-   //Arī kļūdas gadījumā rādīs aprēkināto vērtību, iemesls, kāpēc atstāju opciju redzēt vērtību arī   
-   // kļūdas gadījumā, lai var redzēt, ka tiešām kaut kas ne tā aizgāja.
-  printf("\nFaktoriāls = %d",  fact);
-    
-  }
-  
-   else if (tips == 'l') 
-  {
-     
-     long long  fact = 1;
-     long long ieprieks =1;
-     printf("\nIegaja LONG\n");
-     printf("Rekinam fact skailim %d \n", i);
-     
-     
-     while (i>=1)
-     {
-      fact *= i;
-      printf("Vai aprēķinātā n! dalījums ar n ir vienāds ar (n-1)!? %d \n", (fact/i)==ieprieks);
-      // Ja aprēķinātā n! dalījums ar n NAV VIENĀDS ar (n-1)! tad ir pārsniegta datu tipa robeža
-      if ((fact/i)!=ieprieks) {printf("\nLong datu tipam faktoriālu nevar aprēķināt\nZemak redzam kļūdainais aprkēķins"); break;}
-       printf("iepriekseja vertiba (n-1)! %lld \n", ieprieks);    
-       ieprieks = fact;
-       printf("tekosa vertiba n! %lld \n", fact);
-       i--;
-      
-       
-       
-     }
-   //Arī kļūdas gadījumā rādīs aprēkināto vērtību, iemesls, kāpēc atstāju opciju redzēt vērtību arī   
-   // kļūdas gadījumā, lai var redzēt, ka tiešām kaut kas ne tā aizgāja.
-  printf("\nFaktoriāls = %lld",  fact);
-  }
-  
-  else {printf("Ievadiet korektu datu formātu!");}
-  
-     return 0;
+    noArgumentNoReturn();
+    noArgumentWithReturn()
+    return 0;
 }
