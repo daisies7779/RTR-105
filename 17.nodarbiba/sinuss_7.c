daa2c2200f69f50c0e1e33d495212f0013f0d095
daisies7779 <41288557+daisies7779@users.noreply.github.com>
2021-01-12 23:00:48 +0200

    Update sinuss_7.c

--- 17.nodarbiba/sinuss_7.c
+++ 17.nodarbiba/sinuss_7.c
@@ -6,13 +6,13 @@ double mans_sinuss(double x){
   double a,S;
   int k=0;
   
-  a=pow(-1,k)*pow(x,2*k+1)/(1.);
+  a=pow(-1,k)*pow(x,2*k+1)/(2*1.);
   S=a;
   printf("%.2f\t%8.2f\t%8.2f\n", x, a,S);
   
   while(k<3){
    k++;
-   a = a * (-1)*x*x/((2*k)*(2*k+1));
+   a = a * (-1)*x*x/(4*(2*k)*(2*k+1));
    S = S+a;
    printf("%.2f\t%8.2f\t%8.2f\n", x, a,S); 
   }
@@ -23,10 +23,10 @@ double mans_sinuss(double x){
 
 void main(){
   double x=2.05, y, yy;
-  y=sin(x);
-  printf("standarta funkcijas sin - y=sin(%.2f)=%.2f\n", x, y);
+  y=sin(x/2);
+  printf("standarta funkcijas sin - y=sin(%.2f/2)=%.2f\n", x, y);
   
   yy=mans_sinuss(x);
-    printf("lietotaja funkcijas - y=mans_sinuss(%.2f)=%.2f\n", x, yy);
+    printf("lietotaja funkcijas - y=mans_sinuss(%.2f/2)=%.2f\n", x, yy);
 
 }
