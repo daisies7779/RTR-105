bc7f32945ed00e4e33b2607ccb88087c66e5ff34
daisies7779 <41288557+daisies7779@users.noreply.github.com>
2021-01-14 01:52:45 +0200

    Update sinuss.c

--- darbi/1ld_series/sinuss.c
+++ darbi/1ld_series/sinuss.c
@@ -28,11 +28,11 @@ double mans_sinuss(double x){
 void zimesana(double x){
      
      // 92 is \ backslahs ASCII value 47 / forwardslash
-    
+    //rekurences reizinatajs:  (-1)*x*x/(4*(2*k)*(2*k+1));
     printf("%12c\n",92);
-    printf("%13c    (-1) * x",92);
+    printf("%13c    (-1) * (0.5*x)",92);
     printf("\n sin(%.2f) = >   ______________",x);
-    printf("\n%13c    (-1) * x",47);
+    printf("\n%13c    (2*k+1)! * x",47);
     
 }
 
