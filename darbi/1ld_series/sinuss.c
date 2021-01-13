efae33e526313d01f15a8a5649e8065178037508
daisies7779 <41288557+daisies7779@users.noreply.github.com>
2021-01-14 00:37:17 +0200

    Update sinuss.c

--- darbi/1ld_series/sinuss.c
+++ darbi/1ld_series/sinuss.c
@@ -8,17 +8,17 @@ double mans_sinuss(double x){
   a=pow(-1,k)*pow(x,2*k+1)/(2*1.);
   S=a;
   
-  printf("iteracija\targuments\ta\tSumma\n");
-  printf("%3d %8.2f\t %8.2f\t %.2f\n",k, x, a,S); 
+  printf("Nr.\targuments\ta\tSumma\n");
+  printf("%3d %8.2f\t %8.4f\t %.2f\n",k, x, a,S); 
   
   
   while(k<500){
    k++;
    a = a * (-1)*x*x/(4*(2*k)*(2*k+1));
    S = S+a;
-    if ((k==1)||(k>498))
+    if ((k<6)||(k>498))
    //printf("%d %.2f\t%8.2f\t%8.2f\n",k, x, a,S); 
-   printf("%3d %8.2f\t %8.2f\t %.2f\n",k, x, a,S); 
+   printf("%3d %8.2f\t %8.4f\t %.5f\n",k, x, a,S); 
   }
   
   return S;
@@ -31,10 +31,11 @@ void main(){
   printf("Sin/2 aprekinasana:\n");
   printf("Ievadiet argumentu x (radianos):");
   scanf(" %lf", &x);
-  y=sin(x/2);
-  printf("standarta funkcijas sin(x/2) y=sin(%.2f/2)=%.2f\n", x, y);
+  
   
   yy=mans_sinuss(x);
+  y=sin(x/2);
+  printf("\nstandarta funkcijas sin(x/2) y=sin(%.2f/2)=%.2f\n", x, y);
   printf("lietotaja funkcija y=mans_sinuss(%.2f/2)=%.2f\n", x, yy);
 
 }
