842f1253bc3335c1fc128b099de11e8a22545314
daisies7779 <41288557+daisies7779@users.noreply.github.com>
2021-01-15 02:39:57 +0200

    Update roots.c

--- darbi/2ld_roots/roots.c
+++ darbi/2ld_roots/roots.c
@@ -1,11 +1,16 @@
 #include<stdio.h>
 #include<math.h>
+
+float modified_sin(float x, float A){
+	return sin(x)-A;
+}
+
 int main () {
-	float a=0.01, b=1.5*M_PI, x, delta_x=1.e-3, funkca, funkcb, funkcx;
+	float a=0.01, b=1.5*M_PI, x, delta_x=1.e-3, A=1, funkca, funkcb, funkcx;
 	int k=0;
 	
-	funkca = sin(a);
-	funkcb = sin(b);
+	funkca = modified_sin(a, A);
+	funkcb = modified_sin(b, A);
 	
 	if(funkca*funkcb>0){
 		printf("Intervala [%.2f;%.2f] sin(x) funkcijai", a,b);
@@ -13,26 +18,26 @@ int main () {
 		return 1;
 	}
 	
-	printf("               sin(%7.3f)=%7.3f\t\t\t\t", a,sin(a));
-	printf("sin(%7.3f)=%7.3f\n",b, sin(b));
+	printf("               sin(%7.3f)=%7.3f\t\t\t\t", a,modified_sin(a,A));
+	printf("sin(%7.3f)=%7.3f\n",b, modified_sin(b,A));
 	
 	while((b-a)>delta_x){
 		k++;
 		x=(a+b)/2.;
 		//pie a=0 -> funkca=0 -> reizinajums ir precizi 0 -> b=x
-		if(funkca*sin(x)>0){
+		if(funkca*modified_sin(x,A)>0){
 			a=x;
 		}
 		else {
 			b=x;
 		}
 		
-		printf("%2d. iteracija: sin(%7.3f)=%7.3f\t", k,a,sin(a));
-		printf("sin(%7.3f)=%7.3f\t", x,sin(x));
-		printf("sin(%7.3f)=%7.3f\n", b,sin(b));
+		printf("%2d. iteracija: sin(%7.3f)=%7.3f\t", k,a,modified_sin(a,A));
+		printf("sin(%7.3f)=%7.3f\t", x,modified_sin(x,A));
+		printf("sin(%7.3f)=%7.3f\n", b,modified_sin(b,A));
 	}
 	
- printf("Sakne atrodas pie x=%.3f, jo sin(x) ir %.3f\n", x,sin(x));
+ printf("Sakne atrodas pie x=%.3f, jo sin(x) ir %.3f\n", x,modified_sin(x,A));
 	
  return 0;
  }
