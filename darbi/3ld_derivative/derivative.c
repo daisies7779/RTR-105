2ca260d9b6060b175dd11a11b069a04ef43c6550
daisies7779 <41288557+daisies7779@users.noreply.github.com>
2021-01-16 20:03:07 +0200

    Update derivative.c

--- darbi/3ld_derivative/derivative.c
+++ darbi/3ld_derivative/derivative.c
@@ -24,25 +24,27 @@ int main(){
   //funkcijas vertiba pie argumenta x
   y=sin(x/2);
   
-  y_1_analitiski = 0.5*cos(x/2);
+  y_1_analitiski = 0.5*cos(x/2.);
   
-  y_2_analitiski = -0.25*sin(x/2);
+  y_2_analitiski = -0.25*sin(x/2.);
   
-  y_1_diferencejot = (sin(x/2+delta_x)-sin(x/2))/delta_x);
+  y_1_diferencejot = (sin((x+delta_x)/2.)-sin(x/2.))/delta_x;
   
-  y_2_diferencejot = ((sin(x/2+2*delta_x)-2*sin(x/2+delta_x) -sin(x/2))/pow(delta_x,2));
+  //y_2_diferencejot = ((sin(x/2.+2.*delta_x)-2*sin(x/2.+delta_x) -sin(x/2.))/pow(delta_x,2));
+   
+  y_2_diferencejot = (sin((x+2*delta_x)/2.)-2*sin((x+delta_x)/2.)-sin(x/2.))/pow(delta_x,2);
   
   //fprintf(dataFile,"%.4f     %.4f\n",  x, modified_sin(x,A));
   
   
-  fprintf(dataFile,"%.2lf\t%.2lf\t\t%.2lf\t\t\t%.2lf\t\t\t%.2lf\t\t\t%.2lf\n", x, y, y_1_analitiski, y_2_analitiski, y_1_diferencejot, y_2_diferencejot);
+  fprintf(dataFile,"%.2lf\t%.2lf\t\t%.2lf\t\t\t%.2lf\t\t\t%.2lf\t\t\t%.2lf\n", x, y, y_1_analitiski, y_1_diferencejot, y_2_analitiski,  y_2_diferencejot);
   
    
    
   
   
  //y = sin(x);
- printf("%10.2f\t%10.2f\t%10.2f\n", x,sin(x), (sin(x+delta_x)-sin(x))/delta_x);
+// printf("%10.2f\t%10.2f\t%10.2f\n", x,sin(x), (sin(x+delta_x)-sin(x))/delta_x);
   
   //printf("%10.2f\t%10.2f\n", x,y);
   x+=delta_x;
