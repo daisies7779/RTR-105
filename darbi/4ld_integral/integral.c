a7fe35b5ebcd3bab347725778fe2299b90e25220
daisies7779 <41288557+daisies7779@users.noreply.github.com>
2021-01-16 22:28:24 +0200

    Create integral.c

---
+++ darbi/4ld_integral/integral.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+#include<math.h>
+
+
+int main(){
+ float a, b, x, delta_x, y, y_1_analitiski, y_1_diferencejot, y_2_analitiski, y_2_diferencejot, test;
+ int k=0;
+ 
+ printf("Šī programma aprēķina sin(x/2) prim1, prim2,\n izmantojot gan analītisko formulu, gan diferenic.\n");
+ printf("Ievadiet zemāko robežu: ");
+ scanf(" %f", &a);
+ printf("Ievadiet augstāko robežu: ");
+ scanf(" %f", &b);
+ printf("Ievadiet par cik palielināt x katru ciklu: ");
+ scanf(" %f", &delta_x);
+ 
+ FILE * dataFile;
+ dataFile = fopen("derivative.dat","w");
+ fprintf(dataFile,"  x\t  sin(x/2)  anal_sin\'(x/2)   difer_sin\'(x/2)   anal_sin\'\'(x/2)  difer_sin\'\'(x/2)\n");
+ 
+ while(x<=b){
+  if(x>b) break;
+  
+  //funkcijas vertiba pie argumenta x
+  y=sin(x/2);
+  
+  y_1_analitiski = 0.5*cos(x/2.);
+  
+  y_2_analitiski = -0.25*sin(x/2.);
+  
+  y_1_diferencejot = (sin((x+delta_x)/2.)-sin(x/2.))/delta_x;
+  
+   
+ // y_2_diferencejot = (sin((x+2*delta_x)/2.)-2*sin((x+delta_x)/2.)-sin(x/2.))/pow(delta_x,2);
+  // ar trigonometriskiem parveidojumiem vienkarso lidz cosinus funkcijai un tad tai rekina diferenci
+  y_2_diferencejot  = 0.5*((cos((x+delta_x)/2.)-cos(x/2))/delta_x);
+  
+  
+  
+  fprintf(dataFile,"%.2lf\t%.2lf\t\t%.3lf\t\t\t%.3lf\t\t\t%.4lf\t\t\t%.4lf\n", x, y, y_1_analitiski, y_1_diferencejot, y_2_analitiski,  y_2_diferencejot);
+  
+  x+=delta_x;
+  }
+  
+  //aizver failu
+  fclose(dataFile);
+
+}
