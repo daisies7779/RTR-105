d3fe161252fc62b5fd1e0ba36c91323952e27247
daisies7779 <41288557+daisies7779@users.noreply.github.com>
2021-01-16 16:13:31 +0200

    Update derivative_sinuss.c

--- 17.nodarbiba/derivative_sinuss.c
+++ 17.nodarbiba/derivative_sinuss.c
@@ -8,7 +8,7 @@ void main(){
   x=a;
   while(x<b){
   //y = sin(x);
-  printf("%10.2f\t%10.2f\t%10.2f\n", x,sin(x), (sin(x+delta_x)-sin(x-delta_x))/delta_x);
+  printf("%10.2f\t%10.2f\t%10.2f\n", x,sin(x), (sin(x+delta_x)-sin(x))/delta_x);
   
   //printf("%10.2f\t%10.2f\n", x,y);
   x+=delta_x;
