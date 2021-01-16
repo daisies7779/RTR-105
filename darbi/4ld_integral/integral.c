14e915a9307f9c190d3810e93bdbbb2abc85a57b
daisies7779 <41288557+daisies7779@users.noreply.github.com>
2021-01-17 00:16:26 +0200

    Update integral.c

--- darbi/4ld_integral/integral.c
+++ darbi/4ld_integral/integral.c
@@ -1,18 +1,55 @@
 #include<stdio.h>
 #include<math.h>
 
+void integralRectangle(double a, double b, double iedalja)
+{
+    double x=a, integralis = 0;
+    
+    while(x<b) 
+    { // Taisnstūra metode
+        x+=iedalja;
+        integralis+=(x-a)*(sin(x/2));
+        a=x;
+    }
+       printf("Integralis, izmantojot taisnstura, metodi ir %lf\n", integralis);
+}
+
+
+void integralTrapece(double a, double b, double iedalja)
+{
+    double x = a, integralis = 0;
+    while(x<b)
+    { 
+        x += iedalja;
+        integralis +=(sin(x/2)+sin(a/2))/2*(x-a);
+        a= x;
+    }
+    
+    printf("Integralis, izmantojot trapeces metodi, ir %lf\n", integralis);
+}
+
 
 int main(){
- float a, b, x, delta_x, y, y_1_analitiski, y_1_diferencejot, y_2_analitiski, y_2_diferencejot, test;
- int k=0;
+ double a, b, x, iedalja;
+ int n;
  
- printf("Šī programma aprēķina sin(x/2) prim1, prim2,\n izmantojot gan analītisko formulu, gan diferenic.\n");
+ printf("Šī programma aprēķina sin(x/2) integrāli,\n");
  printf("Ievadiet zemāko robežu: ");
- scanf(" %f", &a);
+ scanf(" %lf", &a);
  printf("Ievadiet augstāko robežu: ");
- scanf(" %f", &b);
- printf("Ievadiet par cik palielināt x katru ciklu: ");
- scanf(" %f", &delta_x);
+ scanf(" %lf", &b);
+ printf("Cik daļās sadalīt intervālu: ");
+ scanf(" %d", &n);
+ 
+ iedalja = (b-a)/n;
+ 
+ //integralRectangle(a,b,iedalja);
+ integralTrapece(a,b,iedalja);
+// integralSimpson(a,b,iedalja);
+ 
+  return 0;
+ 
+ /*
  
  FILE * dataFile;
  dataFile = fopen("derivative.dat","w");
@@ -44,5 +81,6 @@ int main(){
   
   //aizver failu
   fclose(dataFile);
+  */
 
 }
